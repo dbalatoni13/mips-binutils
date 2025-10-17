da8f0151b4a4d5e13f8262a99e4be8a0969b5e87
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2025-10-17 04:29:38 +0200

    Add MacOS patch

--- build-macos.sh
+++ build-macos.sh
@@ -3,6 +3,9 @@ PREFIX="$(pwd)/build"
 mkdir source
 wget -qO- https://mirror.netcologne.de/gnu/binutils/binutils-2.45.tar.xz | tar -xJ -C source --strip-components=1
 cd source
+for patch in ../*.patch; do
+  patch -N -p1 -i "$patch"
+done
 export CFLAGS="-arch arm64 -arch x86_64 -mmacosx-version-min=10.11"
 ./configure --target=mips-linux-gnu --prefix="$PREFIX" --disable-nls --disable-gprof --without-zstd
 make -j$(nproc) configure-host
